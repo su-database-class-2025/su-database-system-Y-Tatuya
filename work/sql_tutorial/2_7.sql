fbabf7a8f94f2e6cf127e3516426bcca4beeb20c
Tatsuya <12345678tatuya@users.noreply.github.com>
2025-12-06 21:54:27 +0900

    [2025-12-06 23TI078: 油井達弥] チュートリアル課題提出

---
+++ work/sql_tutorial/2_7.sql
@@ -0,0 +1,21 @@
+SELECT max(temp_lo) FROM weather;
+
+SELECT city FROM weather WHERE temp_lo = max(temp_lo);
+
+SELECT city FROM weather
+    WHERE temp_lo = (SELECT max(temp_lo) FROM weather);
+
+SELECT city, max(temp_lo)
+    FROM weather
+    GROUP BY city;
+
+SELECT city, max(temp_lo)
+    FROM weather
+    GROUP BY city
+    HAVING max(temp_lo) < 40;
+
+SELECT city, max(temp_lo)
+    FROM weather
+    WHERE city LIKE 'S%'            -- (1)
+    GROUP BY city
+    HAVING max(temp_lo) < 40;
